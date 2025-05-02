06ca092754e82d2649107046efa0d1f3defd20aa
GalLu707 <zasseuwu@gmail.com>
2025-05-02 16:06:06 -0600

    avance de clase

--- semana 2/Laboratorio2Proyectos/src/laboratorio2proyectos/pagos.java
+++ semana 2/Laboratorio2Proyectos/src/laboratorio2proyectos/pagos.java
@@ -13,12 +13,24 @@ public class pagos {
     public static void main(String[]args){
         double pagoPhora = 35.99;
         int HorasAtrabajar= 40;
-        System.out.println("Ingrese la cantidad de horas que trabajo al mes:");
         Scanner obj = new Scanner(System.in);
+        obj.useDelimiter("\n");
+        
+        System.out.println("introduzca su nombre");
+        String Nombre= obj.next();
+        
+        System.out.println("Introduzca su apellido:");
+        String Apellido = obj.next();
+        
+        System.out.println("introduzca su codigo");
+        String Codigo= obj.next();
+        
+        System.out.println("Ingrese la cantidad de horas que trabajo al mes:");
         int HorasMes = obj.nextInt();
+        
         System.out.println("Ingrese la categoria a la que pertenece:");
         int catego = obj.nextInt();
-        obj.useDelimiter("\n");
+        
         int HorasExtras = HorasMes - HorasAtrabajar; 
         
         if(HorasExtras<0){
@@ -27,12 +39,16 @@ public class pagos {
         System.out.println("sus horas extra son:"+ HorasExtras);
         }
         
+        System.out.println("Plantilla \nNombre del empleado:"+ Nombre +"\nApellido:"+ Apellido+ "\nsu codigo es:" + Codigo);
+        
         switch(catego) {
             case 1:
-              // int Hextra=  
+                
+              System.out.println("wiwiwiwi");
                 
                 break;
             case 2:
+                System.out.println("papu pro");
                 break;
             case 3:
                 break;

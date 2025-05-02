90c2a514eb9eeb9c7c79f46e6c47865a32ad984b
GalLu707 <zasseuwu@gmail.com>
2025-05-02 13:03:07 -0600

    avance de clase

--- semana 2/Laboratorio2Proyectos/src/laboratorio2proyectos/Laboratorio2Proyectos.java
+++ semana 2/Laboratorio2Proyectos/src/laboratorio2proyectos/Laboratorio2Proyectos.java
@@ -15,6 +15,8 @@ public class Laboratorio2Proyectos {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        //when haces tus momazos 
+        System.out.println("hola");
     }
     
 }

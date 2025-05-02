b4143e88a7b7ac223e17734831fb3170afb5c075
GalLu707 <zasseuwu@gmail.com>
2025-05-02 15:29:26 -0600

    avance de clase

---
+++ semana 2/Laboratorio2Proyectos/src/laboratorio2proyectos/pagos.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package laboratorio2proyectos;
+
+/**
+ *
+ * @author USER
+ */
+import java.util.Scanner
+public class pagos {
+    public static void main(String[]args){
+        double pagoPhora = 35.99;
+        int HorasAtrabajar= 40;
+        
+        int horaExtra= HorasAtrabajar -;
+    }
+    
+}

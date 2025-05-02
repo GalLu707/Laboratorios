597319922a2ce2bc6b9cbe313208e6b0a15fab1a
GalLu707 <zasseuwu@gmail.com>
2025-05-02 14:35:29 -0600

    avance de clase

---
+++ semana 2/Laboratorio2Proyectos/src/laboratorio2proyectos/fechaass.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package laboratorio2proyectos;
+
+/**
+ *
+ * @author USER
+ */
+public class fechaass {
+    //clases ejercicio 1
+    public static void main(String[]args){
+        
+    }
+    
+}

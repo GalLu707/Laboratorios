094d6a1c74a0d46c58befbd44853668563edc0dc
GalLu707 <zasseuwu@gmail.com>
2025-05-02 15:38:19 -0600

    avance de clase

--- semana 2/Laboratorio2Proyectos/src/laboratorio2proyectos/pagos.java
+++ semana 2/Laboratorio2Proyectos/src/laboratorio2proyectos/pagos.java
@@ -8,13 +8,29 @@ package laboratorio2proyectos;
  *
  * @author USER
  */
-import java.util.Scanner
+import java.util.Scanner;
 public class pagos {
     public static void main(String[]args){
         double pagoPhora = 35.99;
         int HorasAtrabajar= 40;
+        System.out.println("Ingrese la cantidad de horas que trabajo al mes:");
+        Scanner obj = new Scanner(System.in);
+        int HorasMes = obj.nextInt();
+        System.out.println("Ingrese la categoria a la que pertenece:");
+        String catego = obj.next();
+        obj.useDelimiter("\n");
         
-        int horaExtra= HorasAtrabajar -;
+        switch{
+            case 1:
+            case 2:
+            case 3:
+            case 4: 
+                
+        }
+        
+           
+        
+        //int horaExtra= HorasAtrabajar;
     }
     
 }
